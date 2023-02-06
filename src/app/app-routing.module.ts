334bfbbb89dd4049a6dc8dc04f1a1d0badb9fc61
JoshSevy <joshuasevy@outlook.com>
2023-02-05 21:53:20 -0700

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StayTunedComponent } from './homepage/stay-tuned/stay-tuned.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./homepage/stay-tuned/stay-tuned.module').then(
+        (m) => m.StayTunedModule
+      ),
+    component: StayTunedComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/homepage/stay-tuned/stay-tuned.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StayTunedComponent } from './stay-tuned.component';
+
+@NgModule({
+  declarations: [StayTunedComponent],
+  imports: [CommonModule],
+  exports: [StayTunedComponent],
+})
+export class StayTunedModule {}
