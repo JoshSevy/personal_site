90f51bffc2cb89bf666f363a536c113530c672af
JoshSevy <joshuasevy@outlook.com>
2023-02-05 21:53:20 -0700

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SimpleHeaderComponent } from './header/simple-header/simple-header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { StayTunedModule } from './homepage/stay-tuned/stay-tuned.module';
+import { MatCommonModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SimpleHeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCommonModule,
+    StayTunedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
