f04cbb144da4dcf276a726a31e9da40dfc12f1b9
JoshSevy <joshuasevy@outlook.com>
2023-02-06 22:58:48 -0700

    Add fonts init spinner

--- src/app/blog/home/home.component.ts
+++ src/app/blog/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'blog-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class BlogHomeComponent {}
+export class BlogHomeComponent implements OnInit {
+  loading = true;
+
+  constructor() {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  }
+}
