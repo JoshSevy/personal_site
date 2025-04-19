6efcf07663a834edfe3cb8a05be60e4cd8df6551
Joshua Sevy <joshuasevy@outlook.com>
2025-04-18 18:22:13 -0600

    Update scripts

--- src/app/blog-home/blog-post/blog-post.component.ts
+++ src/app/blog-home/blog-post/blog-post.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { BlogService } from '../services/blog.service';
+import { BlogPost } from '../blog-post.model';
+import { AsyncPipe, DatePipe, NgForOf, NgIf } from '@angular/common';
+import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { SanitizeHtmlPipe } from '../../pipes/sanitize-html.pipe';
+import { HighlightCodeDirective } from '../../directives/highlight-syntax.directive';
 
 @Component({
   selector: 'app-blog-post',
-  standalone: true,
-  template: `
-    <div>
-      <h2>Blog Post</h2>
-      <!-- Blog post content will go here -->
-    </div>
-  `
+  imports: [
+    NgIf,
+    AsyncPipe,
+    FormsModule,
+    DatePipe,
+    SanitizeHtmlPipe,
+    HighlightCodeDirective,
+    NgForOf,
+    RouterLink,
+  ],
+  templateUrl: './blog-post.component.html',
 })
-export class BlogPostComponent {}
+export class BlogPostComponent implements OnInit {
+  post$: Observable<BlogPost> | undefined;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogService
+  ) {}
+
+  ngOnInit(): void {
+    const postId = this.route.snapshot.paramMap.get('id');
+    this.post$ = this.blogService.getPostById(postId);
+  }
+}
