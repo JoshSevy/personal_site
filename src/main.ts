ba38e6c152192b62c96d92e1740c27bb0f2b01f7
Joshua Sevy <joshuasevy@outlook.com>
2026-04-03 01:32:06 -0600

    fix dup apollo breaking headers'

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-import { provideZonelessChangeDetection, inject } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
-import { provideApollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideZonelessChangeDetection(),
-    ...appConfig.providers, provideHttpClient(), provideApollo(() => {
-      const httpLink = inject(HttpLink);
-
-      return {
-        link: httpLink.create({
-          uri: '<%= endpoint %>',
-        }),
-        cache: new InMemoryCache(),
-      };
-    })
-    ]
-
-})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
